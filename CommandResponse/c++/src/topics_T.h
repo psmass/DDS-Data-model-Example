cf7cb2841336a0c81460ab2a8e3aeeae77b32808
Paul Schmitt <42389607+psmass@users.noreply.github.com>
2022-05-24 10:53:34 -0400

    chkpt - Template classes compile (wtr class works)

--- CommandResponse/c++/src/topics_T.h
+++ CommandResponse/c++/src/topics_T.h
@@ -25,24 +25,170 @@ namespace MODULE
 // data_processing routines for the concrete class. (Likely we can remove the
 // higher level Reader/Writer classes as they don't add much - as they did for
 // XML Application creation).
-// T is the Topic - e.g. DeviceState
+// T is the Topic - e.g. DeviceState // not actually used inthe template
+// S is the TopicTypeSupport e.g. DeviceStatusTypeSupport
 // R is the Reader for the specific topic - e.g. DeviceStateReader
-// template<typename T, typename R> class TopicRdr;// T is the Topic - e.g. DeviceState
-template<typename T, typename R>
+template<typename T, class S, typename R>
 class TopicRdr : public Reader {
     public:
         TopicRdr(
             DDSDomainParticipant * participant,
             DDSSubscriber * subscriber,
-            const char* qos_profile);
+            const char* qos_profile,
+            const char* topic_name,
+            const char* topic_rdr_name);
         ~TopicRdr (void){};
 
         void Handler(void);
-        void process_data(T * data); // specific topic to process
+        // override for specific topic process_data 
+        virtual void process_data(T * data) { // default prints the data
+            MODULE::DeviceStateTypeSupport::print_data(data); 
+        };
         
     protected:
         R * topicReader;
 };
+
+
+template<class T, class S, class R>
+TopicRdr<T,S,R>::TopicRdr(
+            DDSDomainParticipant * participant,
+            DDSSubscriber * subscriber,
+            const char* qos_profile,
+            const char* topic_name,
+            const char* topic_rdr_name) 
+            : Reader(participant, subscriber, topic_name, topic_rdr_name ) {
+
+        DDS_ReturnCode_t retcode, retcode1, retcode2;
+
+        // Register the specific datatype to use when creating the Topic
+        // this calls a type specific type, so is required to be done in the specific
+        // type Reader/Writer. The remaining work is done in the base class
+        this->topicTypeName=((char *)S::get_type_name());
+        retcode =
+            S::register_type(participant, this->topicTypeName);
+        if (retcode != DDS_RETCODE_OK) {
+            throw std::invalid_argument("Reader thread: type name error");
+        }
+
+        // Create a Topic with a name and a datatype
+        DDSTopic *topic = participant->create_topic(
+            this->topicName,
+            this->topicTypeName,
+            DDS_TOPIC_QOS_DEFAULT,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
+        if (topic == NULL) {
+            throw std::invalid_argument("Reader thread: create topic error");
+        }
+
+        // This DataReader reads data on "Example MODULE_DeviceState" Topic
+        DDSDataReader *untyped_reader = subscriber->create_datareader_with_profile(
+            topic,
+            MODULE::CMD_RSP_QOS_LIBRARY,
+            qos_profile,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
+        if (untyped_reader == NULL) {
+            throw std::invalid_argument("Reader thread: create data reader error");
+        }
+
+        // Narrow casts from an untyped DataReaderto a Reader of your type 
+        this->topicReader=(R::narrow(untyped_reader));
+            if (this->topicReader == NULL) {
+                throw std::invalid_argument("Reader thread: get narrow Reader error");
+            }  
+
+        // Create read condition
+        this->readCondition=this->topicReader->create_readcondition(
+            DDS_NOT_READ_SAMPLE_STATE,
+            DDS_ANY_VIEW_STATE,
+            DDS_ANY_INSTANCE_STATE);
+
+        //  Get & Set status conditions
+        this->statusCondition=this->topicReader->get_statuscondition();
+        retcode = this->statusCondition->set_enabled_statuses(DDS_SUBSCRIPTION_MATCHED_STATUS);  
+
+        /* Attach Read Conditions */
+        retcode1 = this->waitset->attach_condition(this->readCondition);
+
+        /* Attach Status Conditions */
+        retcode2 = this->waitset->attach_condition(this->statusCondition);
+        if ((retcode != DDS_RETCODE_OK) | (retcode1 != DDS_RETCODE_OK) | (retcode2 != DDS_RETCODE_OK)) {
+            throw std::invalid_argument("Reader thread: get/attach condition error");
+        }
+}
+
+template<class T, class S, class R>
+void TopicRdr<T,S,R>::Handler()
+{
+        T data_seq;
+        DDS_SampleInfoSeq info_seq;
+        DDSConditionSeq active_conditions_seq;
+        DDS_ReturnCode_t retcode;
+        DDS_Duration_t wait_duration = {1,0}; // timeout wait to ensure running
+
+        std::cout << "Device State Reader Handler Executing" << std::endl; 
+
+        while (!application::shutdown_requested) {
+            // Wait 4 seconds for data 
+            retcode = this->waitset()->wait(active_conditions_seq, wait_duration);
+            // waitset.wait(dds::core::Duration(4));
+            if (retcode == DDS_RETCODE_TIMEOUT) {  
+                // std::cout << "Reader thread: Wait timed out!! No conditions were triggered" << std::endl;
+                // put thead health check here since we verified we are running
+                continue;
+            } else if (retcode != DDS_RETCODE_OK) {
+                throw std::invalid_argument("Reader handler: wait returned error ");
+            }
+
+            int active_conditions = active_conditions_seq.length();
+
+            for (int i = 0; i < active_conditions; ++i) {
+                if (active_conditions_seq[i] == this->statusCondition()) {
+                    // Get the status changes so we can check which status
+                    // ondition triggered.
+                    DDS_StatusMask triggeredmask =
+                            this->topicReader->get_status_changes();
+
+                    // Subscription matched 
+                    if (triggeredmask & DDS_SUBSCRIPTION_MATCHED_STATUS) {
+                        DDS_SubscriptionMatchedStatus st;
+                        this->topicReader->get_subscription_matched_status(st);
+                        std::cout << this->topicName << "Reader Pubs: " 
+                        << st.current_count << "  " << st.current_count_change << std::endl;
+                    }
+                } else if (active_conditions_seq[i] == this->readCondition) { 
+                    // Get the latest samples
+                    retcode = this->topicReader->take(
+                                data_seq, info_seq, DDS_LENGTH_UNLIMITED,
+                                DDS_ANY_SAMPLE_STATE, DDS_ANY_VIEW_STATE, DDS_ANY_INSTANCE_STATE);
+
+                    if (retcode == DDS_RETCODE_OK) {
+                        // we've got some data for what ever topic we recieved, figure that out, make an
+                        // internal variable change as a result (if that's the case) and respond accordingly 
+                        // (with a RequestResponse not an On Change Topic. On Change topics trigger from the 
+                        // main loop as you peruse through internal variables that you see have changed as a
+                        // result of a request or other internal event.
+                        for (int i = 0; i < data_seq.length(); ++i) {
+                            if (info_seq[i].valid_data) {  
+                                this->process_data(&data_seq[i]);
+                            }
+                        }
+                    } else if (retcode == DDS_RETCODE_NO_DATA) {
+                        continue;
+                    } else {
+                        throw std::invalid_argument("Reader handler: read data error ");
+                    }
+                    retcode = this->topicReader->return_loan(data_seq, info_seq);
+                    if (retcode != DDS_RETCODE_OK) {
+                        throw std::invalid_argument("Reader handler: return_loan error  ");
+                    }  
+                }
+            }
+        } //while
+        std::cout << this->topicName<< " Reader Handler shutting down" << std::endl; 
+}
 
 // T is the Topic - e.g. DeviceState
 // S is the TopicSupport type e.g. DeviceStatusTypeSupport
